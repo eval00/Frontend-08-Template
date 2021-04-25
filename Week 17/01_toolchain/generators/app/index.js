57f7454850528eca960aebda901c49c696cd6d41
eval00 <1240227693@qq.com>
2021-04-25 21:23:51 +0800

    初始化工具Yeoman（二）

--- Week 17/01_toolchain/generators/app/index.js
+++ Week 17/01_toolchain/generators/app/index.js
@@ -5,11 +5,50 @@ module.exports = class extends Generator {
         super(args, opts);
     }
 
-    method1() {
-        this.log('method 1 just ran');
+    //  1. interactions
+    async method1() {
+        return;
+        const answers = await this.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "Your project name",
+                default: this.appname // Default to current folder name
+            },
+            {
+                type: "confirm",
+                name: "cool",
+                message: "Would you like to enable the Cool feature?"
+            }
+        ]);
+       
+       this.log('app name', answers.name);
+       this.log('cool feature', answers.cool);
     }
 
-    method2() {
-        this.log('method 2 just ran');
+    // 2. Interacting with the file system
+    async step1() {
+        return;
+        this.fs.copyTpl(
+            this.templatePath('t.html'),
+            this.destinationPath('public/index.html'),
+            { title: 'Templating with Yeoman' }
+        );
+    }
+
+    // 3. Managing Dependencies
+    initPackage() {
+        const pkgJson = {
+            devDependencies: {
+                eslint: '^3.15.0'
+            },
+            dependencies: {
+                react: '^16.2.0'
+            }
+        };
+    
+        // Extend or create package.json file in destination path
+        this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
+        // this.npmInstall();
     }
 };
