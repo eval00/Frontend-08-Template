9039ed6be0a131413dc5a737a5cc625db314212f
eval00 <1240227693@qq.com>
2021-04-26 20:13:00 +0800

    code coverage

--- Week 18/01_test-demo/test/test.js
+++ Week 18/01_test-demo/test/test.js
@@ -1,6 +1,9 @@
 var assert = require('assert');
 
-import { add } from '../add.js';
+// var add = require('../add.js').add;
+// var mul = require('../add.js').mul;
+
+import { add, mul } from '../add.js';
 
 describe('add function testing', function() {
     it('1+2 should be 3', function() {
@@ -9,5 +12,9 @@ describe('add function testing', function() {
     
     it('-5+2 should be -3', function() {
         assert.equal(add(-5, 2), -3);
+    });
+    
+    it('-5*2 should be -10', function() {
+        assert.equal(mul(-5, 2), -10);
     });
 })
